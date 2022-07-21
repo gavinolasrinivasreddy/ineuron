31792aa41b3955059959f1dc99a5fe8db64f05d7
gsreddy <gavinolasrinivasreddy@gmail.com>
2022-07-21 11:59:55 +0530

    first ever commit

---
+++ mongotest.py
@@ -0,0 +1,13 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://gsreddyphysics:reddy1985@cluster1.luyvz.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+d = {
+     "name":"gsreddy",
+     "email":"gsreddyphysics@gmail.com",
+     "surname" : "gavinolla"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
